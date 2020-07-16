29595781444f25e981dbbd380d81132560c58197
joaomarcos11 <joaomarcos11.tech@gmail.com>
2020-07-16 12:04:35 -0300

    Selecao de botaos com JS no create-point

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -38,3 +38,52 @@ function getCities(event) {
 }
 
 document.querySelector('select[name=uf]').addEventListener('change', getCities);
+
+// Itens de coleta
+// pegar todos os li's
+const itemsToCollect = document.querySelectorAll('.items-grid li');
+
+for (const item of itemsToCollect) {
+  item.addEventListener('click', handleSelectedItem);
+}
+
+const collectedItems = document.querySelector('input[name=items]');
+
+let selectedItems = [];
+
+function handleSelectedItem(event) {
+  const itemLi = event.target;
+
+  // adicionar ou remover uma classe com JS
+  itemLi.classList.toggle('selected');
+
+  const itemId = itemLi.dataset.id;
+
+  // verificar se existem itens selecionados, se sim
+  // pegar os itens selecionados
+
+  const alreadySelected = selectedItems.findIndex((item) => {
+    const itemFound = item == itemId; // isso será true ou false
+    return itemFound;
+  });
+
+  // se já estiver selecionado,
+  if (alreadySelected >= 0) {
+    // tirar da seleção
+    const filteredItems = selectedItems.filter((item) => {
+      const itemIsDifferent = item != itemId; // false
+      return itemIsDifferent;
+    });
+
+    selectedItems = filteredItems;
+  } else {
+    // se não estiver selecionado
+    // adicionar à seleção
+    selectedItems.push(itemId);
+  }
+
+  // console.log(selectedItems);
+
+  // atualizar o campo escondido com os items selecionados
+  collectedItems.value = selectedItems;
+}
