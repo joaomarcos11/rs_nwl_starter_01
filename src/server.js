22237678492d81f58c286f766af43a7e71645bc5
joaomarcos11 <joaomarcos11.tech@gmail.com>
2020-07-17 09:10:07 -0300

    Implementacao de template engines com nunjucks

--- src/server.js
+++ src/server.js
@@ -6,12 +6,25 @@ const PORT = 3333;
 // configurando pasta publica
 server.use(express.static('public'));
 
+// template engine
+const nunjucks = require('nunjucks');
+nunjucks.configure('src/views', {
+  express: server,
+  noCache: true,
+});
+
 server.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+  // res.sendFile(__dirname + '/views/index.html');
+  return res.render('index.html', { title: 'Um título' });
 });
 
 server.get('/create-point', (req, res) => {
-  res.sendFile(__dirname + '/views/create-point.html');
+  // res.sendFile(__dirname + '/views/create-point.html');
+  return res.render('create-point.html');
+});
+
+server.get('/search', (req, res) => {
+  return res.render('search-results.html');
 });
 
 server.listen(PORT, () => console.log(`Servidor iniciado na porta ${PORT}`));
